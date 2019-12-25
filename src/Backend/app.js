de882dfc1f84bc191addc7c98a3f5e805d325725
Evil Artist <loc.nguyenxuan99@outlook.com>
2019-12-25 18:14:24 +0700

    Create first module using http module

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,1 +1,10 @@
 // Main application
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.write("Welcome to E-Space!"); 
+    console.log("response writes \"Welcome to E-Space!\"");
+    response.end();
+}).listen(2383); 
+  
+console.log("Sever is listening at port 2383");
