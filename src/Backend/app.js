91d67573419d8fef651e96a7db4d85619a9f5c04
Evil Artist <loc.nguyenxuan99@outlook.com>
2020-01-01 07:43:01 +0700

    Add express

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,61 +1,17 @@
-// Main application
-var http = require('http');
-var url = require('url');
+var express = require('express');
 
-http.createServer(function (request, response) {
-    var method = request.method;
-    if (method == "GET") {
-        handleGETMethod(response);
-    }
-    else if (method == "POST") {
-        handlePOSTMethod(request, response);
-    }
-    else{
-        response.end();
-    }
-    
-}).listen(2383); 
+var app = express();
 
-function handleGETMethod(response) {
-    response.statusCode = 404;
-    var ts = Date.now();
-    var date = new Date(ts);
-    response.write(date.toTimeString());
-    response.end();
-}
+app.get('/', function(request, response){
+   response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+   response.write("Welcome you to E-Space!");
+   response.end();
+});
 
-function handlePOSTMethod(request, response) {
-    var body = "";
-    var address = url.parse(request.url, true);
-    request.on('data', function (data) {
-        body += data;
-    });
-    request.on('end', function () {
-        var data = JSON.parse(body);
-        var x = data.x;
-        var y = data.y;
-        if (address.path == "/add") {
-            var z = x + y;
-            response.write(x + " + " + y + " = " + z);
-        }
-        else if (address.path == "/subtract") {
-            var z = x - y;
-            response.write(x + " - " + y + " = " + z);
-        }
-        else if (address.path == "/multiple") {
-            var z = x * y;
-            response.write(x + " * " + y + " = " + z);
-        }
-        else if (address.path == "/devide") {
-            var z = x / y;
-            response.write(x + " / " + y + " = " + z);
-        }
-        else{
-            response.statusCode = 404;
-            response.write(address.path);
-            console.log(data);
-        }
-        response.end();
-    });
-}
+app.get('/slogan', function(request, response){
+   response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+   response.write("E-Space: Không gian sáng tạo");
+   response.end();
+});
 
+app.listen(2383);
