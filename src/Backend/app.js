c06d5ad8055318b70d11cb081fb5ac9375c9a481
Evil Artist <loc.nguyenxuan99@outlook.com>
2020-01-03 21:32:54 +0700

    Bài 1.1

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,43 +1,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(bodyParser.text())
 
-function eSpaceMiddleware(request, response, next) {
-    
-    console.log("Get request");
-    console.log(request.body);
-    var x = parseInt(request.body.x);
-    var y = parseInt(request.body.y);
-    request.x = x;
-    request.y = y;
-    next();
-}
-app.use(eSpaceMiddleware);
- 
-app.post('/add', function(request, response){
-    
-    var z = request.x + request.y;
-    response.send(request.x + " + " + request.y + " = " + z);
-});
-app.post('/subtract', function(request, response){
-    var z = request.x - request.y;
-    response.send(request.x + " - " + request.y + " = " + z);
-});
-app.post('/multiple', function(request, response){
-    var z = request.x * request.y;
-    response.send(request.x + " * " + request.y + " = " + z);
-});
-app.post('/divide', function(request, response){
-    var z = request.x / request.y;
-    response.send(request.x+ " / " + request.y + " = " + z);
-});
- 
-app.get('/', function(request, response){
-   response.sendFile('./views/index.html', { root: __dirname });
-});
+var ledRequest = [];
+app.get("/", function (request, response) {
+    response.sendFile('views/index.html', {root: __dirname});
+})
 
-app.listen(2383);
+app.post("/led", function (request, response) {
+    console.log(request.body.status);
+    
+    request.body.status.forEach(led => {
+        var index = parseInt(led);
+        ledRequest[index] = "on";        
+    });
+    if (request.body.status) {
+        ledRequest = request.body.status;
+    }
+    else {
+        ledRequest = "off";
+    }
+    response.send("OK");
+})
+
+app.get("/led", function (request, response) {
+    response.send(ledRequest);
+    ledRequest = "NoRequest";
+    
+})
+app.listen(2382);
+
