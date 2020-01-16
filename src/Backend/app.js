44120f8a8b9f6c7b7a103b8c39f23bbeb9381589
Evil Artist <loc.nguyenxuan99@outlook.com>
2020-01-16 21:52:17 +0700

    Bai2

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -4,31 +4,61 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-var ledRequest = [];
+var ledRequest = ["NoRequest","NoRequest","NoRequest","NoRequest","NoRequest"];
+
 app.get("/", function (request, response) {
+    console.log("getHompage");
     response.sendFile('views/index.html', {root: __dirname});
 })
 
-app.post("/led", function (request, response) {
-    console.log(request.body.status);
-    
-    request.body.status.forEach(led => {
-        var index = parseInt(led);
-        ledRequest[index] = "on";        
-    });
+app.post("/led/:name/:id", function (request, response) {    
+    var id = parseInt(request.params.id) - 1; 
+    var name = request.params.name
+    var device = getDeviceByName(name);
+    ledRequest =  device.ledRequest;
     if (request.body.status) {
-        ledRequest = request.body.status;
+        ledRequest[id] = "on"
     }
     else {
-        ledRequest = "off";
+        ledRequest[id] = "off";
     }
-    response.send("OK");
+    response.send("Led " + (id + 1) + ": " + ledRequest[id])
 })
 
-app.get("/led", function (request, response) {
-    response.send(ledRequest);
-    ledRequest = "NoRequest";
-    
+app.get("/led/:name", function (request, response) {
+    var requestStatus = "";
+    var device = getDeviceByName(name);
+    ledRequest = device.request;
+    for (const led of ledRequest) {
+        if (led == "on") {
+            requestStatus += "1";
+        }
+        else if (led == "off") {
+            requestStatus += "0";
+        }
+        else{
+            requestStatus += "-";
+        }
+    } 
+    response.send(requestStatus);
+    ledRequest = ["NoRequest", "NoRequest", "NoRequest", "NoRequest", "NoRequest"];
 })
-app.listen(2382);
+
+app.get("/led/:name/:id", function (request, response) {
+    var id = parseInt(request.params.id) - 1;  
+    response.send(ledRequest[id]);
+    ledRequest[id] = "NoRequest";
+})
+
+var devices = [
+    { "name": "device1", "html": "device1.html"},
+    { "name": "device2", "html": "device2.html"},
+]
+app.get("/:name", function (request, response) {
+
+    var name = request.params.name;  
+    var device = getDeviceByName(name);
+    response.sendFile('views/' + device.html, {root: __dirname});
+})
+app.listen(2383);
 
--- src/Devices/esp8266.ino
+++ src/Devices/esp8266.ino
@@ -8,52 +8,60 @@ const char* password = "12346789";
 IPAddress server(192,168,1,8);
 HTTPClient http;
 WiFiClient client;
+uint8_t LEDs[5] = {3,4,5,6,7};
 void setup()
 {
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        pinMode(LEDs[i], OUTPUT);
+    }
+    
     Serial.begin(115200);
     WiFi.begin(ssid, password);
     WiFi.mode(WIFI_STA);
+    char a[3] = "AB";
+    int c = (int) strtol(a, 0, 16);
+    Serial.println(c);
+    delay(10000);
     while (WiFi.status() != WL_CONNECTED) 
     {    
         delay(100);
         Serial.println("Connecting..");
     } 
     Serial.println("[Connected]");
+    
+
 }
 
 void loop()
 {
-    //GETMethod();
     if (client.connect(server, 2383))
     {
-        // GETMethod();
-        // delay(1000);
-        POSTMethod("add");
-        delay(1000);
-    }
-    Serial.println("\n[Cannot connect retry]");
-    delay(1000);
+        GETMethod();
+        delay(3000);
+    }  
 }
 
 void GETMethod()
 {
-    Serial.println("[Sending a request]");
-    client.print(String("GET /") + " HTTP/1.1\r\n" +
-                "Host: 192.168.1.8:2383/\r\n" +
-                "Connection: close\r\n" +
-                "\r\n"
-                );
-    Serial.println("[Response:]");
-    while (client.connected() || client.available())
+     Serial.println("Begin Get data");
+    String address = "http://192.168.1.8:2383/led";
+    http.begin(address);    
+    int httpCode = http.GET();            
+    String payload = http.getString(); 
+    char status[6];
+    payload.toCharArray(status, 6, 0);
+    for (uint8_t i = 0; i < 5; i++)
     {
-        if (client.available())
+        if (status[i] == '1')
         {
-            String line = client.readStringUntil('\n');
-            Serial.println(line);
+            digitalWrite(LEDs[i], HIGH);
+        }
+        else if(status[i] == '0')
+        {
+            digitalWrite(LEDs[i], LOW);
         }
     }
-    client.stop();
-    Serial.print("\n[Disconnected]");
 }
 
 void POSTMethod(String path)
